68475084bd03e91753517e5fe27f57223b54e8d9
Henry Blyth <blyth.henry@gmail.com>
2022-03-06 01:31:33 +0000

    Revert "Improve starting look"
    This reverts commit 64cca7685d39732a549c04d64dee6824ebbbd10a.

--- main.js
+++ main.js
@@ -94,8 +94,6 @@ function run() {
     postcodeInput.value = storedPostcode;
     setLoading(false);
     renderFeed(JSON.parse(storedRes), storedPostcode);
-  } else {
-    document.querySelectorAll('.hide-when-unstarted').forEach(el => /** @type {HTMLElement} */ (el).hidden = true);
   }
 
   const urlPostcode = new URLSearchParams(location.search).get('postcode');
@@ -177,7 +175,6 @@ function run() {
     }
 
     clearError();
-    document.querySelectorAll('.hide-when-unstarted').forEach(el => /** @type {HTMLElement} */ (el).hidden = false);
 
     // debugElement.innerHTML = '';
     // resultDateEl.innerHTML = '';
