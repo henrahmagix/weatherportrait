e848a5e89ded9ec6ecc94f928d9d93e2f8486e58
Henry Blyth <blyth.henry@gmail.com>
2022-03-06 00:25:54 +0000

    Store response so refresh is immediate

--- main.js
+++ main.js
@@ -25,6 +25,9 @@ function filmFromSunninessAndUV(sunniness, uv) {
   }
 }
 
+const JSON_STORAGE_KEY = 'weatherportrait_res';
+const POSTCODE_STORAGE_KEY = 'weatherportrait_url';
+
 const messages = [
   'looks to be a nice dose of',
   'how about a spot of',
@@ -73,6 +76,8 @@ function run() {
   const resultItemsEl     = /** @type {HTMLDListElement} */ (document.getElementById('result_items'));
   const resultSourceEl    = /** @type {HTMLElement} */      (document.getElementById('result_source'));
 
+  const kindError = "sometimes the beeb's RSS feed is a bit 🤷, not sure why, keep trying again and hopefully it'll work 😊";
+
   postcodeForm.addEventListener('submit', event => {
     event.preventDefault();
     event.stopPropagation();
@@ -85,8 +90,15 @@ function run() {
     }
   });
 
+  const storedRes = localStorage.getItem(JSON_STORAGE_KEY);
+  const storedPostcode = localStorage.getItem(POSTCODE_STORAGE_KEY);
+  if (storedRes && storedPostcode) {
+    postcodeInput.value = storedPostcode;
+    renderFeed(JSON.parse(storedRes), storedPostcode);
+  }
+
   const urlPostcode = new URLSearchParams(location.search).get('postcode');
-  if (urlPostcode) {
+  if (urlPostcode && urlPostcode !== storedPostcode) {
     postcodeInput.value = urlPostcode;
     fetchAndDisplay(urlPostcode);
   }
@@ -107,6 +119,10 @@ function run() {
     errorsElement.textContent = '';
   }
 
+  function feedUrl(postcode) {
+    return `https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/${postcode}`;
+  }
+
   /** @param {string} postcode */
   function fetchAndDisplay(postcode) {
     postcode = postcode.toLowerCase();
@@ -114,19 +130,9 @@ function run() {
     clearError();
     setLoading(true);
 
-    const kindError = "sometimes the beeb's RSS feed is a bit 🤷, not sure why, keep trying again and hopefully it'll work 😊";
-
-    const feedUrl = `https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/${postcode}`;
-    xhrGet(feedUrl)
+    xhrGet(feedUrl(postcode))
       .then(res => {
         setLoading(false);
-
-        // debugElement.innerHTML = '';
-        resultTitleEl.innerHTML = '';
-        // resultDateEl.innerHTML = '';
-        resultCopyrightEl.innerHTML = '';
-        resultItemsEl.innerHTML = '';
-        resultSourceEl.innerHTML = '';
 
         if (!res) {
           setError(`No response - ${kindError}`);
@@ -146,84 +152,101 @@ function run() {
         }
 
         const xmlJson = parseXmlToObject(res.responseXML, ['item']);
-        const weatherValues = xmlJson && xmlJson.rss && xmlJson.rss.channel;
-        if (!weatherValues) {
-          setError(`Unexpected response - ${kindError}`);
-          console.error(`Unable to parse weather values from RSS feed:\n${res.responseXML.documentElement.outerHTML}`);
-          return;
-        }
-
-        resultElement.lang = weatherValues.language && weatherValues.language['#text']; // if null, attr will be removed, which is good, so it defaults to inheritance
-
-        resultTitleEl.textContent = weatherValues.title && weatherValues.title['#text'];
-        // resultDateEl.textContent = weatherValues.pubDate && `Report made at ${weatherValues.pubDate['#text']}`;
-        resultCopyrightEl.textContent = weatherValues.copyright && weatherValues.copyright['#text'];
-
-        if (weatherValues.link) {
-          resultSourceEl.textContent = 'Source for weather data: ';
-          const sourcePageLink = document.createElement('a');
-          const sourceFeedLink = document.createElement('a');
-
-          sourceFeedLink.href        = feedUrl;
-          sourceFeedLink.textContent = 'feed';
-          sourceFeedLink.ariaLabel   = 'Source RSS feed for the weather data';
-          resultSourceEl.appendChild(sourceFeedLink);
-
-          resultSourceEl.append(' | ');
-
-          // When inputting a lowercase postcode, the RSS feed returns a page
-          // link with the postcode in uppercase, which fails to find a forecast
-          // when you visit it. So until that gets fixed (or maybe we're using
-          // the RSS feed wrong?) then we build our own source link.
-          // sourcePageLink.href        = weatherValues.link['#text'];
-          sourcePageLink.href        = `https://www.bbc.co.uk/weather/${postcode}`;
-          sourcePageLink.textContent = 'page';
-          sourceFeedLink.ariaLabel   = 'Source page for the weather data';
-          resultSourceEl.appendChild(sourcePageLink);
-        }
-
-        if (weatherValues.item) {
-          weatherValues.item.forEach(item => {
-            let /** @type {string} */ title = item.title && item.title['#text'].split(',')[0]; // pulls out the name of the day and overview; ignores temperatures
-            if (!title) {
-              return;
-            }
-
-            let titleParts = title.split(':');
-            let day        = titleParts[0];
-            let sunniness  = titleParts[1] && titleParts[1].trim();
-
-            let /** @type {string} */ uvString = item.description && item.description['#text'];
-            let uvMatch = uvString.match(/UV[^,.]*(\d+)/);
-            let uvNumber = uvMatch && uvMatch[1];
-
-            if (title && uvNumber != null) {
-              const listTitle = document.createElement('dt');
-              const listItem = document.createElement('dd');
-              listTitle.textContent = day;
-              listItem.innerHTML = `${lovelyMessage()} <em>${filmFromSunninessAndUV(sunniness, parseInt(uvNumber[1], 10))}</em>${Math.random() > 0.5 ? '?' : ''}`;
-              resultItemsEl.appendChild(listTitle);
-              resultItemsEl.appendChild(listItem);
-            }
-          });
-        } else {
-          const listItem = document.createElement('dd');
-          listItem.textContent = 'No forecast days found';
-          resultItemsEl.appendChild(listItem);
-        }
-
-        if (!resultItemsEl.textContent || resultItemsEl.textContent.length === 0) {
-          setError(`Something went wrong - ${kindError}`);
-          console.error(`Unable to parse weather items from RSS feed:\n${res.responseXML.documentElement.outerHTML}`);
-        } else {
-          clearError();
-          resultElement.hidden = false;
-        }
+        localStorage.setItem(JSON_STORAGE_KEY, JSON.stringify(xmlJson));
+        localStorage.setItem(POSTCODE_STORAGE_KEY, postcode);
+        renderFeed(xmlJson, postcode);
       })
       .catch(err => {
         setError(`Something went wrong - ${kindError}`);
         console.error(err);
       });
+  }
+
+  /**
+   * @param {object} xmlJson
+   * @param {string} postcode
+   */
+  function renderFeed(xmlJson, postcode) {
+    const weatherValues = xmlJson && xmlJson.rss && xmlJson.rss.channel;
+    if (!weatherValues) {
+      setError(`Unexpected response - ${kindError}`);
+      console.error(`Unable to parse weather values from RSS feed:\n${JSON.stringify(xmlJson, null, 2)}`);
+      return;
+    }
+
+    // debugElement.innerHTML = '';
+    resultTitleEl.innerHTML = '';
+    // resultDateEl.innerHTML = '';
+    resultCopyrightEl.innerHTML = '';
+    resultItemsEl.innerHTML = '';
+    resultSourceEl.innerHTML = '';
+
+    resultElement.lang = weatherValues.language && weatherValues.language['#text']; // if null, attr will be removed, which is good, so it defaults to inheritance
+
+    resultTitleEl.textContent = weatherValues.title && weatherValues.title['#text'];
+    // resultDateEl.textContent = weatherValues.pubDate && `Report made at ${weatherValues.pubDate['#text']}`;
+    resultCopyrightEl.textContent = weatherValues.copyright && weatherValues.copyright['#text'];
+
+    if (weatherValues.link) {
+      resultSourceEl.textContent = 'Source for weather data: ';
+      const sourcePageLink = document.createElement('a');
+      const sourceFeedLink = document.createElement('a');
+
+      sourceFeedLink.href        = feedUrl(postcode);
+      sourceFeedLink.textContent = 'feed';
+      sourceFeedLink.ariaLabel   = 'Source RSS feed for the weather data';
+      resultSourceEl.appendChild(sourceFeedLink);
+
+      resultSourceEl.append(' | ');
+
+      // When inputting a lowercase postcode, the RSS feed returns a page
+      // link with the postcode in uppercase, which fails to find a forecast
+      // when you visit it. So until that gets fixed (or maybe we're using
+      // the RSS feed wrong?) then we build our own source link.
+      // sourcePageLink.href        = weatherValues.link['#text'];
+      sourcePageLink.href        = `https://www.bbc.co.uk/weather/${postcode}`;
+      sourcePageLink.textContent = 'page';
+      sourceFeedLink.ariaLabel   = 'Source page for the weather data';
+      resultSourceEl.appendChild(sourcePageLink);
+    }
+
+    if (weatherValues.item) {
+      weatherValues.item.forEach(item => {
+        let /** @type {string} */ title = item.title && item.title['#text'].split(',')[0]; // pulls out the name of the day and overview; ignores temperatures
+        if (!title) {
+          return;
+        }
+
+        let titleParts = title.split(':');
+        let day        = titleParts[0];
+        let sunniness  = titleParts[1] && titleParts[1].trim();
+
+        let /** @type {string} */ uvString = item.description && item.description['#text'];
+        let uvMatch = uvString.match(/UV[^,.]*(\d+)/);
+        let uvNumber = uvMatch && uvMatch[1];
+
+        if (title && uvNumber != null) {
+          const listTitle = document.createElement('dt');
+          const listItem = document.createElement('dd');
+          listTitle.textContent = day;
+          listItem.innerHTML = `${lovelyMessage()} <em>${filmFromSunninessAndUV(sunniness, parseInt(uvNumber[1], 10))}</em>${Math.random() > 0.5 ? '?' : ''}`;
+          resultItemsEl.appendChild(listTitle);
+          resultItemsEl.appendChild(listItem);
+        }
+      });
+    } else {
+      const listItem = document.createElement('dd');
+      listItem.textContent = 'No forecast days found';
+      resultItemsEl.appendChild(listItem);
+    }
+
+    if (!resultItemsEl.textContent || resultItemsEl.textContent.length === 0) {
+      setError(`Something went wrong - ${kindError}`);
+      console.error(`Unable to parse weather items from RSS feed JSON:\n${JSON.stringify(xmlJson, null, 2)}`);
+    } else {
+      clearError();
+      resultElement.hidden = false;
+    }
   }
 }
 
